d097af88fe0c4e87a66521f7621f666ab222f823
QzCrane <107861762+QzCrane@users.noreply.github.com>
2026-07-07 01:46:10 +0800

    test: isolate semantic group a

---
+++ tests/integration/semantic-group-a.test.ts
@@ -0,0 +1,5 @@
+await import('./semantic-contract.test.ts');
+await import('../unit/engineering-ir.test.ts');
+await import('../unit/semantic-contract-ir.test.ts');
+await import('../unit/semantic-contract-responsibility.test.ts');
+await import('../unit/semantic-lowering.test.ts');
